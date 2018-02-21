d15e7c10243037dd0b8316a757acdd6eabb39214
Carl Shackelford <cs@shacks.com>
2018-02-21 09:36:10 -0600

    Testing Git

---
+++ HI PI Bye/MainPage.xaml.vb
@@ -0,0 +1,17 @@
+﻿' The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+''' <summary>
+''' An empty page that can be used on its own or navigated to within a Frame.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        'test
+        txtbox1.text = "Hello Pi!" '
+    End Sub
+
+    Private Sub but_2_Click(sender As Object, e As RoutedEventArgs) Handles but_2.Click
+        txtbox1.text = "GoodBye Pi!"
+    End Sub
+End Class
---
+++ Hello Bye Pi/MainPage.xaml.vb
@@ -0,0 +1,21 @@
+﻿' The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+''' <summary>
+''' An empty page that can be used on its own or navigated to within a Frame.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        textbox1.text = "Hello Pi"
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Button_Click_2(sender As Object, e As RoutedEventArgs)
+        textbox1.text = "Goodbye Pi"
+    End Sub
+End Class
+
